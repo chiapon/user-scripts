523eb0fdbb1f5c15b2710c8d1431c4c2c4c8fde2
Alex Lin <chiapon@gmail.com>
2021-01-28 15:38:59 +0800

    User script for medium.com full view.

---
+++ full-medium.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Medium Hackd
+// @version      1.00.03
+// @description  Unlock Medium article limitation by leveraging Medium Hackd server https://radiant-brushlands-42789.herokuapp.com/
+// @author       Alex Lin
+// @include      https://medium.com/*
+// @include      https://towardsdatascience.com/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_info
+// @grant        GM.getValue
+// @grant        GM.setValue
+// @grant        GM.xmlHttpRequest
+// @grant        GM.info
+// ==/UserScript==
+
+
+if(document.getElementById('paywall-upsell-button-upgrade')) {
+  let url = 'https://radiant-brushlands-42789.herokuapp.com/' + document.URL.replace(/^https?\:\/\//i, "");
+  console.log('Redirect to ' + url);
+  document.location.href = url
+}
